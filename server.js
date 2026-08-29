bb73dc7643b01f53d45240e2e3e1df3ea165d620
talkeasenow <edu.pradeep10@gmail.com>
2026-08-29 09:49:33 +0530

    Add route to serve index.html file

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app=express();
 const server=http.createServer(app);
 const io=new Server(server);
 app.use(express.static(path.join(__dirname,"public")));
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"index.html")));
 
 let talkers=[];
 const listeners=new Map();
